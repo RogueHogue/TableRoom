c675150c0e6723eeed7c1552827c2c4ad7b7ea05
BuildTools <unconfigured@null.spigotmc.org>
2018-02-26 16:38:56 -0500

    #2

--- server.js
+++ server.js
@@ -15,15 +15,6 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 
 console.log('HELLOOOOO');
 
-// error handling
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
-});
-
-initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
-});
 
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
