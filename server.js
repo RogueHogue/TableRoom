77353d7fd41a3837cb6826ed7b6ecb2f66e4bc47
BuildTools <unconfigured@null.spigotmc.org>
2018-02-26 17:45:24 -0500

    Final Server Test

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
-/*
+
 if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
   var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
       mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
@@ -56,7 +56,7 @@ var initDb = function(callback) {
     console.log('Connected to MongoDB at: %s', mongoURL);
   });
 };
-*/
+
 app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
